e6f8869cb9d44d6dddd60b9233a729235d8af020
levieriksson <levieriksson@hotmail.com>
2026-01-27 18:45:45 +0100

    Vercel update test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main style={{ padding: "2rem" }}>
-      <h1>🚧 Portfolio under construction</h1>
+      <h1>🚧 Portfolio under construction 🚧</h1>
       <p>Next.js + TypeScript + Vercel</p>
     </main>
   );
