25f3ac37e1ed6710ac17fbd6d15d65bec5b0c392
levieriksson <levieriksson@hotmail.com>
2026-05-18 13:42:48 +0200

    feat(ai): add traffic brief backend skeleton

--- apps/flighttracker/flighttracker-api/FlightTracker.Backend/Program.cs
+++ apps/flighttracker/flighttracker-api/FlightTracker.Backend/Program.cs
@@ -75,6 +75,7 @@ builder.Services.AddDbContext<FlightDbContext>(options =>
     options.UseNpgsql(connectionString, x => x.MigrationsAssembly("FlightTracker.Data")));
 
 builder.Services.AddHttpClient("opensky");
+builder.Services.AddScoped<AiTrafficBriefService>();
 
 builder.Services.ConfigureHttpJsonOptions(o =>
 {
